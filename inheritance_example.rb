e936e45b1f04b1a9b4cb52ff22c60870fadf0949
dani <dani.zaghian@gmail.com>
2021-01-07 10:35:08 -0800

    adds inheritance example with super

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,5 +1,5 @@
 class Vehicle
-  def initialize
+  def initialize(input_options)
     @speed = 0
     @direction = 'north'
   end
@@ -18,18 +18,37 @@ class Vehicle
 end
 
 class Car < Vehicle
+  attr_accessor :fuel, :make, :model
+
+  def initialize(input_options)
+    super
+    @fuel = input_options[:fuel]
+    @make = input_options[:make]
+    @model = input_options[:model]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  attr_accessor :type, :weight
+
+  def initialize(input_options)
+    super
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-bike = Bike.new
-car = Car.new
+bike = Bike.new({type: "Mountain", weight: 25})
+car = Car.new(fuel: "Diesel", make: "Ford", model: "F150")
 car.honk_horn
 bike.ring_bell
+puts car.make
+puts bike.type
